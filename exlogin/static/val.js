ab7f13adb0289c2555f15e39d157acf8061a0361
Mahmoud <mhd-10@hotmail.com>
2019-01-22 10:03:19 +0300

    update

--- exlogin/static/val.js
+++ exlogin/static/val.js
@@ -6,20 +6,19 @@ function validation() {
     let nationality = document.getElementById("nationalty")
     let daraga = document.getElementById("daraga")
     let deyana = document.getElementById("deyana")
-    x = valfn(firstname.value)
-    y = valpass(password.value, confirm.value)
-    z = valusername(username.value)
-    // console.log(x)
-    console.log(y)
-    // console.log(z)
-    
+    fname = valfn(firstname.value)
+    passwd = valpass(password.value, confirm.value)
+    usernm = valusername(username.value)
+
+
+    test(fname, passwd, usernm)
 }
 
 function valfn(firstname) {
     if (/\d/.test(firstname)) {
         return false
     } else {
-        return firstname.length > 8 ? false:true
+        return firstname.length > 8 ? false : true
         // (condition)'true/false' ?  (if true) : (if false )
     }
 }
@@ -30,10 +29,20 @@ function valusername(username) {
 
 function valpass(password, confirm) {
     r = /^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])([A-Za-z\d@#$%^&£*\-_+=[\]{}|\\:',?/`~"();!]|\.(?!@)){8,16}$/
-    return password == confirm ? r.test(password) :false
+    return password == confirm ? r.test(password) : false
     // if (password == confirm) {
     //     return r.test(password)
     // } else {
     //     return false
     // }
+}
+
+function test(x, y, z) {
+
+    console.log(x, y, z)
+     if (x == true && y == true && z == true) {
+        window.location.href = "index.html";
+
+    }
+
 }
